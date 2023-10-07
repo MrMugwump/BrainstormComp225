632c4838c19add113299c6bede8ba26196c7aa28
MrMugwump <bbridenb@macalester.edu>
2023-10-07 17:51:03 -0500

    got a progress bar working, will add comments to explain everything soon

--- src/app/timer.tsx
+++ src/app/timer.tsx
@@ -2,42 +2,56 @@ import React from "react"
 import { useState, useEffect } from 'react';
 import "./timerStyle.css"
 
-const ProgressBar = ({ progress }:any) => ( 
-    <div className="progressbar">
-      <div className="progress" style={{ width: `${progress}%`}}>
-      </div>
+const ProgressBar = ({ progress, interval }:any) => ( 
+  <div className="progressbar">
+    <div className="progress" style={{ 
+      width: `${progress}%`,
+      transitionDuration: `${interval}ms`,
+      }}>
     </div>
-  )
+  </div>
+)
 
 const SECOND = 1_000;
 const MINUTE = SECOND * 60;
 const HOUR = MINUTE * 60;
 const DAY = HOUR * 24;
 
-export default function TimerModule({deadline, timerLength, interval, timeEnded}:any){
-    var date = new Date(deadline);
-    const [timespan,setTimespan] = useState(timerLength + date.getTime() - Date.now());
-    useEffect(()=>{ // code adapted from https://dev.to/yuridevat/how-to-create-a-timer-with-react-7b9 
-        const intervalId = setInterval(()=>{
-            setTimespan((_timespan)=>{
-                if(_timespan <= 0){
-                  return timerLength;
-                }
-                else{
-                  return _timespan - interval*0.1;
-                }
-            });
-        }, interval*0.1);
+export default function TimerModule({timerLength, interval, timeEnded}:any){
+  const [timespan,setTimespan] = useState(timerLength);
+  const [keyID,setKeyID] = useState(0);
+  useEffect(()=>{
+    const intervalId = setInterval(()=>{
+        setTimespan((_timespan: number)=>{
+            if(_timespan <= 0){
+              setKeyID((_keyID)=>_keyID+1);
+              return timerLength;
+            }
+            else{
+              return _timespan - interval*0.1;
+            }
+        });
+    }, interval*0.1);
 
-        return () => {clearInterval(intervalId)}
-        
-    },[interval]);
+    return () => {clearInterval(intervalId)}
+    
+  },[interval]);
 
-    useEffect(()=>{
-        setTimespan(timerLength);
-    },[timeEnded]);
+  useEffect(()=>{
+      setTimespan(timerLength);
+      
+      // return ()=>setTimespan(-21);
+  },[timeEnded]);
 
-    return(
-        <><p>{timespan/SECOND}</p></>
-    );
+  return(
+      <>
+        <p>{timespan/SECOND}</p>
+        <hr/>
+        <ProgressBar 
+          key = {keyID}
+          progress={10*timespan/SECOND}
+          interval={interval}
+          />
+      </>
+  );
 }
