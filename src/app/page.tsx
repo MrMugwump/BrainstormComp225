64502b899690360414c165b41804768f1ce389f2
Henry Skalla <hcskalla@gmail.com>
2023-10-09 15:20:33 -0500

    updated whats on the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,43 +4,8 @@ import Image from 'next/image'
 import React from 'react';
 import { useState } from 'react';
 import TimerModule from './timer';
+import EquationDisplay from './equationDisplay';
 
-function MathProblem() {
-  enum Symbol {
-    Plus="+",
-    Minus="-",
-    Multiply="*",
-  }
-
-  var symbolNum: Number = Math.floor(Math.random()*3)
-  var symbol: Symbol
-  if(symbolNum===0) {
-    symbol=Symbol.Plus
-  } else if(symbolNum===1) {
-    symbol=Symbol.Minus
-  } else {
-    symbol=Symbol.Multiply
-  }
-  var numFirst: Number = Math.floor(Math.random()*20)
-  var numSecond: Number = Math.floor(Math.random()*20)
-  var equation: String = numFirst.toString() +" "+ symbol.toString() +" "+ numSecond.toString()
-
-  var solution: Number
-  if(symbol===Symbol.Plus) {
-    solution=+numFirst + +numSecond
-  } else if(symbol===Symbol.Minus) {
-    solution=+numFirst - +numSecond
-  } else {
-    solution=+numFirst * +numSecond
-  }
-
-  var output: String
-  output = "Question: "+equation+" Solution: "+solution
-
-  return (
-  output  
-);
-}
 
 
 export default function Home() {
@@ -57,14 +22,10 @@ export default function Home() {
         <h1>
           Brainstorm Game
         </h1>
-          <form action="subpage.tsx">
-            <label htmlFor="fname">Answer: </label>
-            <input type="text" id="fname" name="fname"></input>
-            <br></br><br></br>
-            <input type="submit" value="Submit"></input>
-          </form>
+        <p>Equation display:</p>
         <br></br>
-        <MathProblem></MathProblem>
+        <EquationDisplay></EquationDisplay>
+
         <hr/>
         <TimerModule 
         deadline = {deadline}
