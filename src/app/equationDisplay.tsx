282a5da8249b7692c80612c85e20b0b073afde4a
MrMugwump <bbridenb@macalester.edu>
2023-10-11 15:23:28 -0500

    got Equation Display to move input field around, autofocuses, has timer, does not reset timer upon correct answer

--- src/app/equationDisplay.tsx
+++ src/app/equationDisplay.tsx
@@ -1,34 +1,52 @@
+import { Console } from "console";
 import React from "react"
 import { useState, useEffect } from 'react';
+import TimerModule from "./timer";
+import "./equationDisplayStyle.css"
 
 export default function EquationDisplay({}:any){
-    const equation: [firstVar:number, operation:String, secondVal:number, answer:any] = [1,'+',2,3];
-    const inputArray = [false,false,false,true];
-    const [inputVal0,setInputVal0] = useState('');
-    const [inputVal2,setInputVal2] = useState('');
-    const [inputVal3,setInputVal3] = useState('null');
+    const equation: [firstVar:number, secondVal:number, answer:any] = [1,2,3];
+    var operation = '+';
+    const inputArray = [false,false,false];
+    const [inputVal,setInputVal] = useState('');
     const [isCorrect, setIsCorrect] = useState('incorrect');
-
+    const [location,setLocation] = useState(generateRandomInteger(0,2));
+    const [key, setKey] = useState(0);
+    const [timeEnded,setTimeEnded] = useState(1);
+    const [interval,setInterval] = useState(100); 
+    const [timeLength,setTimeLength] = useState(10000);
+    console.log("test")
+    //print("test");
+    inputArray[location] = true;
     useEffect(()=>{
-        if(inputVal3==equation[3]){
-            //regenerate number
+        if(inputVal==equation[location]){
+            inputArray[location] = false;
+            setLocation(generateRandomInteger(0,2));
+            inputArray[location] = true;
             setIsCorrect('correct!');
+            setKey((_key)=>_key+1);
+            setInputVal('');
         }
         else{
             setIsCorrect('incorrect');
         }
-    }, [inputVal3]);
+    }, [inputVal]);
     return(<>
-        <table>
+        <table key = {key} className="EquationDisplay">
         <tbody>
             <tr>
-                <td><NumberSlot number = {equation[0]} isInput = {inputArray[0]} inputVal={setInputVal0}/></td>
-                <td><NumberSlot number = {equation[1]} isInput = {inputArray[1]}/></td>
-                <td><NumberSlot number = {equation[2]} isInput = {inputArray[2]} inputVal={setInputVal2}/></td>
+                <td><NumberSlot number = {equation[0]} isInput = {inputArray[0]} inputVal={setInputVal}/></td>
+                <td><p>{operation}</p></td>
+                <td><NumberSlot number = {equation[1]} isInput = {inputArray[1]} inputVal={setInputVal}/></td>
                 <td>=</td>
-                <td><NumberSlot number = {equation[3]} isInput = {inputArray[3]} inputVal={setInputVal3}/></td>
-                <td><p>Input = {inputVal3}</p></td>
+                <td><NumberSlot number = {equation[2]} isInput = {inputArray[2]} inputVal={setInputVal}/></td>
+                <td><p>Input = {inputVal}</p></td>
                 <td><p>Correct = {isCorrect}</p></td>
+                <td><p>location = {location}</p></td>
+                <td className="timercell"><TimerModule
+                    timerLength={timeLength}
+                    timeEnded={timeEnded}
+                    interval={interval}/></td>
             </tr>
         </tbody>
         </table>
@@ -37,7 +55,13 @@ export default function EquationDisplay({}:any){
 
 function NumberSlot({number = 0, isInput = false, inputVal}:any){
     if (isInput){
-        return(<><input onChange={e=>{inputVal(e.target.value)}}/></>)
+        return(<><input onChange={e=>{inputVal(e.target.value)}} autoFocus={true}/></>)
     }
     return(<><p>{number}</p></>)
+}
+
+function generateRandomInteger(min: number, max: number){ // Taken from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+    min = Math.ceil(min);
+    max = Math.floor(max)+1; //+1 to make max inclusive inclusive
+    return Math.floor(Math.random() * (max - min) + min); // The maximum and the minimum are inclusive
 }
--- src/app/timer.tsx
+++ src/app/timer.tsx
@@ -15,6 +15,7 @@ const ProgressBar = ({ progress, interval }:any) => (
 const SECOND = 1_000; // 1000ms = 1s
 
 /**
+ * NOTE: pauses for one interval length upon instantiation, fix in future?
  * @param timerLength - length of the timer
  * @param interval - what chunk of time we want to decrement by
  * @param timeEnded - if set to true, TimerModule will reset the progress bar
@@ -25,7 +26,7 @@ export default function TimerModule({timerLength, interval, timeEnded}:any){
   const [timespan,setTimespan] = useState(timerLength);
   const [keyID,setKeyID] = useState(0); // used to reset the progress bar
 
-  useEffect(()=>{ 
+  useEffect(()=>{
     const intervalId = setInterval(()=>{
       setTimespan((_timespan: number)=>{
         if(_timespan <= 0){
@@ -33,10 +34,10 @@ export default function TimerModule({timerLength, interval, timeEnded}:any){
           return timerLength;
         }
         else{
-          return _timespan - interval*0.1; //decrements timer by 1/10th of a second
+          return _timespan - interval; //decrements timer by 1/10th of a second
         }
       });
-    }, interval*0.1); // pauses for a tenth of a second
+    }, interval); // pauses for a tenth of a second
 
     return () => {clearInterval(intervalId)} // don't actually know what this does, prob parallel processing stuff
     
