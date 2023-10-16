4d17602ff75ec77ac1580efbe482d49cb3c1a711
Henry Skalla <hcskalla@gmail.com>
2023-10-16 17:02:49 -0500

    States functionality for gamescene

--- src/app/gameScene.tsx
+++ src/app/gameScene.tsx
@@ -1,7 +1,46 @@
 import React from "react"
 import { useState, useEffect } from 'react';
 import EquationDisplay from "./equationDisplay";
+import "./gameSceneStyle.css"
+
+enum GameState {
+    Start = 0,
+    Play = 1,
+    End = 2,
+}
 
 export default function GameScene() {
-    return (<div></div>);
+    const [gameState, setGameState] = useState(GameState.Start);
+
+    const StartScreen = () => (
+        <div className="startScreen">
+            Press here to start the game.
+            <br/><br/>
+            <button className="startButton" onClick={()=>{
+                setGameState(GameState.Play);
+            }}>Begin Game</button>
+        </div>
+    )
+    
+    const PlayScreen = () => (
+        <div className="playScreen">
+            Playing the game.
+        </div>
+    )
+    
+    const EndScreen = () => (
+        <div className="endScreen">
+            End screen text.
+        </div>
+    )
+
+    if(gameState==GameState.Start) {
+        return (StartScreen());
+    } else if(gameState==GameState.Play) {
+        return (PlayScreen());
+    } else if(gameState==GameState.End) {
+        return (EndScreen());
+    } else {
+        return <></>;
+    }
 }
