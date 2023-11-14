72abc2b8a91ab63f56ca58c8e09456fbd2c689bc
June Graff <junegraff11@gmail.com>
2023-11-14 10:47:38 -0600

    Added hearts, WIP implementation

--- src/app/lives.tsx
+++ src/app/lives.tsx
@@ -5,9 +5,34 @@ import { useState, useEffect } from 'react';
  * Graphical display of the user's lives number.
  */
 export default function LivesDisplay({setLivesRemaining, livesRemaining}:any) {
+    if(livesRemaining === 3) {
+        return (
+            <div>
+                <img src="heartMC.png" alt="3 lives remaining" className = "Heart1" id = "Heart1"></img>
+                <img src="heartMC.png" className = "Heart2" id = "Heart2"></img>
+                <img src="heartMC.png" className = "Heart3" id = "Heart3"></img>
+            </div>
+        )
+    } else if(livesRemaining === 2) {
+        return (
+            <div>
+                <img src="heartMC.png" alt="2 lives remaining" className = "Heart1" id = "Heart1"></img>
+                <img src="heartMC.png" className = "Heart2" id = "Heart2"></img>
+                <img src="emptyHeartMC.png" className = "Heart3" id = "Heart3"></img>
+            </div>
+        )
+    } else if(livesRemaining === 1) {
+        return (
+            <div>
+                <img src="heartMC.png" alt="1 life remaining" className = "Heart1" id = "Heart1"></img>
+                <img src="emptyHeartMC.png" className = "Heart2" id = "Heart2"></img>
+                <img src="emptyHeartMC.png" className = "Heart3" id = "Heart3"></img>
+            </div>
+        )
+    }
     return (
         <p className = "LivesDisplay" id = "LivesDisplay">
-            Number of lives remaining: {livesRemaining}
+            An error has occurred
         </p>
     )
 }
