b24337fdb821f254308de5762b9790444f946ad2
June Graff <junegraff11@gmail.com>
2023-11-15 14:52:16 -0600

    Added hearts
    Now with 100% more Minecraft

--- src/app/lives.tsx
+++ src/app/lives.tsx
@@ -1,38 +1,25 @@
 import React from "react"
 import { useState, useEffect } from 'react';
+import fullHeart from './heartMC.png';
+import emptyHeart from './emptyHeartMC.png';
 
 /**
  * Graphical display of the user's lives number.
  */
 export default function LivesDisplay({setLivesRemaining, livesRemaining}:any) {
-    if(livesRemaining === 3) {
-        return (
-            <div>
-                <img src="heartMC.png" alt="3 lives remaining" className = "Heart1" id = "Heart1"></img>
-                <img src="heartMC.png" className = "Heart2" id = "Heart2"></img>
-                <img src="heartMC.png" className = "Heart3" id = "Heart3"></img>
-            </div>
-        )
-    } else if(livesRemaining === 2) {
-        return (
-            <div>
-                <img src="heartMC.png" alt="2 lives remaining" className = "Heart1" id = "Heart1"></img>
-                <img src="heartMC.png" className = "Heart2" id = "Heart2"></img>
-                <img src="emptyHeartMC.png" className = "Heart3" id = "Heart3"></img>
-            </div>
-        )
-    } else if(livesRemaining === 1) {
-        return (
-            <div>
-                <img src="heartMC.png" alt="1 life remaining" className = "Heart1" id = "Heart1"></img>
-                <img src="emptyHeartMC.png" className = "Heart2" id = "Heart2"></img>
-                <img src="emptyHeartMC.png" className = "Heart3" id = "Heart3"></img>
-            </div>
-        )
-    }
     return (
-        <p className = "LivesDisplay" id = "LivesDisplay">
-            An error has occurred
-        </p>
+        <div>
+            {nextHeart(1, livesRemaining)}
+            {nextHeart(2, livesRemaining)}
+            {nextHeart(3, livesRemaining)}
+        </div>
     )
+}
+
+function nextHeart(pos:number, livesRemaining:number) {
+    if(pos > livesRemaining) {
+        return <img src={emptyHeart.src} className="EmptyHeart" id="EmptyHeart"/>;
+    } else {
+        return <img src={fullHeart.src} alt="Heart" className="FullHeart" id="FullHeart"/>;
+    }
 }
