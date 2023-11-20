9aff4bc500199e6959b633fd821d28572678e688
MrMugwump <bbridenb@macalester.edu>
2023-11-20 15:21:48 -0600

    made answerbox a little prettier

--- src/app/player-info-components/answerBox.tsx
+++ src/app/player-info-components/answerBox.tsx
@@ -1,14 +1,20 @@
-import React from "react"
+import React, { useState } from "react"
 
 /*
  * Graphical text box element. Takes in player input and translates it to the "userInput" variable.
  */
 export default function AnswerBox({setUserInput,listOfAnswers}:any) {
+    const [width, setWidth] = useState('1ch');
+    function onInput(input:string){
+        setUserInput(input);
+        setWidth(input.length+'ch')
+    }
     return (
             <input
             className="AnswerBox"
             id="AnswerBox"
-            onChange={e=>{setUserInput(e.target.value);}}
+            onChange={e=>{onInput(e.target.value)}}
+            style={{width:`${width}`}}
             autoFocus={true} /*When instantiated, cursor auto selects.*/ />
     )
 }
