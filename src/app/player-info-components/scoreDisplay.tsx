122fd90bf9c8d5fd564de8c0ac793633ef342840
MrMugwump <bbridenb@macalester.edu>
2023-12-01 15:14:15 -0600

    added small animations to score

--- src/app/player-info-components/scoreDisplay.tsx
+++ src/app/player-info-components/scoreDisplay.tsx
@@ -1,15 +1,33 @@
 import React from "react"
 import './scoreDisplayStyle.css'
-
+import { motion, useAnimation } from "framer-motion";
+import { useEffect } from 'react';
 /*
  * Graphical text element that presents the user's score.
  */
 export function ScoreDisplay({score}:any){
-    function calcScore(score:number){
+    const controls = useAnimation();
+
+    const animations = {
+        increaseScore: {
+            fontSize: ['100%','110%','100%'],
+            textShadow: ['#60d056 0px 0px 0px,#60d056 0px 0px 0px', '#60d056 0px 0px 0.3em, #60d056 0px 0px 0.3em','#60d056 0px 0px 0px,#60d056 0px 0px 0px'],
+            transition: {duration:0.5, ease:'linear'}
+        }
+
     }
+
+    useEffect(()=>{
+        controls.start('increaseScore');
+    },[score]);
+
     return(<>
         <div id="score-display">
-            <p>Score: {score}</p>
+            <p style={{fontSize:'50%'}}>Score</p>
+            <motion.p 
+                animate={controls}
+                variants={animations}>{score}</motion.p>
+            
         </div>
     </>)
 }
